988bef70d00d768d66a4ee45f07c09c19bf00d77
briancheldelin <brianc@acloudy.dev>
2024-12-04 16:44:22 +0000

    checkpoint

--- aoc24/day3/main.go
+++ aoc24/day3/main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"bufio"
 	"io"
+	"log/slog"
 	"regexp"
 	"utility"
 )
 
-const FIND_REG = `(mul\(\d+,\d+\))`
+const FIND_REG_PART1 = `(mul\(\d+,\d+\))`
+const FIND_REG_PART2 = `(mul\(\d+,\d+\))`
 const PARSE_REG = `mul\((\d+),(\d+)\)`
 
-func find(inputReader io.Reader) []string {
-
+func find(inputReader io.Reader, regexString string) []string {
 	scanner := bufio.NewScanner(inputReader)
-
-	re := regexp.MustCompile(FIND_REG)
-
+	re := regexp.MustCompile(regexString)
 	hits := make([]string, 0, 10)
 
 	for scanner.Scan() {
@@ -30,15 +29,49 @@ func parse(operations []string) [][]int {
 	parsedOperations := make([][]int, 0, len(operations))
 	re := regexp.MustCompile(PARSE_REG)
 
-	for i, operation := range operations {
-		matches := re.FindAllString(operation, -1)
-		matchesInt := utility.AtoiSlice(matches)
-		parsedOperations[i] = append(parsedOperations[i], matchesInt...)
-
+	for _, operation := range operations {
+		matches := re.FindStringSubmatch(operation)
+		matchesInt := utility.AtoiSlice(matches[1:])
+		parsedOperations = append(parsedOperations, matchesInt)
 	}
 
+	return parsedOperations
+}
+
+func multiplySum(operations [][]int) (sum int) {
+	for _, op := range operations {
+		sum += multiply(op)
+	}
+	return
+}
+
+func multiply(op []int) (result int) {
+	result = 1
+	for _, op := range op {
+		result *= op
+	}
+	return result
+}
+
+func partOne() {
+	inputReader := utility.GetInputReader()
+	foundOperations := find(inputReader, FIND_REG_PART1)
+	multiplicationSets := parse(foundOperations)
+	multplicationSum := multiplySum(multiplicationSets)
+
+	slog.Info("calculated result for input", "sum", multplicationSum)
+}
+
+func partTwo() {
+	inputReader := utility.GetInputReader()
+	foundOperations := find(inputReader, FIND_REG_PART2)
+	multiplicationSets := parse(foundOperations)
+	multplicationSum := multiplySum(multiplicationSets)
+
+	slog.Info("calculated result for input", "sum", multplicationSum)
 }
 
 func main() {
-	// inputReader := utility.GetInputReader()
+	partOne()
+	partTwo()
 }
--- aoc24/day3/main_test.go
+++ aoc24/day3/main_test.go
@@ -5,15 +5,48 @@ import (
 	"testing"
 )
 
+const TEST_STRING = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
 func TestFind(t *testing.T) {
-	testString := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+	inputReader := strings.NewReader(TEST_STRING)
 
-	inputReader := strings.NewReader(testString)
-
-	foundOperations := find(inputReader)
+	foundOperations := find(inputReader, FIND_REG_PART1)
 
 	if len(foundOperations) != 4 {
 		t.Error("did not get any hits when we expected some")
 	}
 
+}
+
+func TestParse(t *testing.T) {
+	inputReader := strings.NewReader(TEST_STRING)
+	foundOperations := find(inputReader, FIND_REG_PART1)
+	multiplicationSets := parse(foundOperations)
+
+	if len(multiplicationSets) != 4 {
+		t.Error("did not get any multiplication sets")
+	}
+}
+
+func TestMultiplySum(t *testing.T) {
+	inputReader := strings.NewReader(TEST_STRING)
+	foundOperations := find(inputReader, FIND_REG_PART1)
+	multiplicationSets := parse(foundOperations)
+
+	multplicationSum := multiplySum(multiplicationSets)
+
+	if multplicationSum == 0 {
+		t.Error("did not get an expected result")
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	testOp := []int{1, 2}
+	expected := 2
+
+	result := multiply(testOp)
+
+	if result != expected {
+		t.Errorf("did not get expected result: result=%d, expected=%d", result, expected)
+	}
 }
